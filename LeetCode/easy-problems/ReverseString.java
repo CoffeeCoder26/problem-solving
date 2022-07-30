0db89914811541ba11303a1c5ec2e780fecf77d7
Karim Abou Agiza <60415348+karimabouagiza@users.noreply.github.com>
2022-07-30 19:58:12 +0200

    Add files via upload
    adding solution to some easy problems

---
+++ LeetCode/easy-problems/ReverseString.java
@@ -0,0 +1,15 @@
+package leetcode.easyproblems;
+
+public class ReverseString {
+    public static void reverseString(char[] s) {
+
+        for (int i = s.length-1; i>=0;i--){
+            System.out.print("["+s[i] +",]");
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] letters = {'H', 'e', 'l', 'l', 'o'};
+        reverseString(letters);
+    }
+}
---
+++ LeetCode/easy-problems/ToLowerCase.java
@@ -0,0 +1,12 @@
+package leetcode.easyproblems;
+
+public class ToLowerCase {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        ToLowerCase toLowerCase = new ToLowerCase();
+        System.out.println(toLowerCase.toLowerCase("LOVELY"));
+    }
+}
