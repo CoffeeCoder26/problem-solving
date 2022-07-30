19e0721898c7db06e1b4018bd011607e52fe0d76
Karim Abou Agiza <60415348+karimabouagiza@users.noreply.github.com>
2022-07-30 19:51:11 +0200

    Add files via upload

---
+++ Leetcode/study-plan/easy-problems/LengthOfLastWord.java
@@ -0,0 +1,27 @@
+package leetcode.easyproblems;
+
+public class LengthOfLastWord {
+
+    public int lengthOfLastWord(String s) {
+        int lenWord = 0;
+        boolean isTrue =false;
+        String str = s.trim();
+        for (int i =str.length()-1; i>=0;i--){
+            if (Character.isLetter(str.charAt(i))){
+               isTrue = true;
+               lenWord++;
+            }else{
+                if(isTrue){
+                    return lenWord;
+                }
+            }
+        }
+        return lenWord;
+
+    }
+
+    public static void main(String[] args) {
+        LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+        System.out.println(lengthOfLastWord.lengthOfLastWord("   fly me   to   the moon  "));
+    }
+}
